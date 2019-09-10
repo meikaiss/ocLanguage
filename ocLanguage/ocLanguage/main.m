f7d87e15fe10d6f1c9266d7a1bda5b3a88515278
meikai <347287812@qq.com>
2019-09-10 23:55:54 +0800

    创建纯oc语法、不带iOS框架的研究库

---
+++ ocLanguage/ocLanguage/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocLanguage
+//
+//  Created by meikai on 2019/9/10.
+//  Copyright © 2019 meikai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
